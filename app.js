80e9c89e09cde5195d1b6ca5c5e2968a0c29fdab
Musawar13 <musawir.akhter@gmail.com>
2020-06-14 05:56:20 -0700

    javascript assignmemt

---
+++ app.js
@@ -0,0 +1,297 @@
+        //  javascript exrcise No:1 ALERT
+// 1. Write a script to greet your website visitor using JS alert box
+
+//alert ("Hello! Good Morning");
+
+// Write a script to display following message on your web page:
+
+//alert ("Error! Please enter valid password.");
+
+// 3. Write a script to display following message on your webpage:
+
+//alert ('Wellcome to JS land...\n happy coding!');
+
+// 4. Write a script to display following messages in sequence:
+ //alert ("wellcome to js land...");
+
+
+
+
+ //5. Generate the following message through browser’s
+//developer console:
+
+console.log("Hello.. can i run JS through web browser console");
+console.log("yes you can use");
+
+  //  javascript exrcise No:2 VARIABLE
+
+  //TASK NO.1 DECLARE A VARIABLE CALLED A USER NAME
+
+  var username;
+  //TASK NO.2 DECLARE A VARIABLE CALLED MYNAME ASSIGN IT A STRING
+  //var myName = "Musawar Akhtar";
+
+  // TASK NO.3 WRITE A SCRIPT
+  // a) Declare a JS variable, titled message.
+  // b) Assign “Hello World” to variable message
+ //c) Display the message in alert box.
+
+ var message = "HELLO WORLD";
+ alert(message);
+
+  // TASK NO.4 WRITE A SCRIPT TO SAVE STUDERNT'S BIOS DATA IN JS VARIABLE AND SHOWS IN 
+  //ALERT BOX
+  var name = "jhone doe \n"; 
+  var age = "15 years old \n" ;
+  var skill = "certified application developer";
+  var bioData = name + age + skill;
+  alert(bioData);
+
+    // TASK NO.5 WRITE A SCRIPT TO DISSPLAY A FOLLOWING ALERT USING ONE 
+     // JS VARIABEL
+//      var pizza = 'PIZZA';
+//      pizza = x++;
+//      alert("pizza" + x++);
+
+       // TASK NO.6 DECLARE A VARIABLE CALLED EMAIL AND ASSIGN AN YOUR EMAIL IN STRING
+       // THAT SHOWS IN ALERT BOX.
+       var email = "musawir.akhter@gmail.com";
+       alert(email);
+
+         // TASK NO.7 DECLARE A VARIABLE CALLED BOOKS AND ASSIGN A VALUE 
+         //"A SMARTER WAY TO LEARN JAVASCRIPT "
+          var book = "A SMARTER WAY TO LEARN JAVASCRIPT";
+          alert(book);
+
+            // TASK NO.8 WRITE A SCRIPT TO DISPLAY IN BROWSER
+            var script ="Yah! I can write HTML content through JS "
+            document.write(script  +"<br>");
+
+            //   // TASK NO.9 
+            var string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+            alert(string);
+
+            //          javascript exrcise No:3 Variable For Numbers
+            
+             // TASK NO.1 Declare a variable called age & assign it to your age.
+             // show in alert box.
+
+             var age = "I am a 15 years old";
+             alert (age);
+
+               // TASK NO.2 Declare & initialize a variable to keep track of how many
+              // times a visitor has visited a web page. Show his/her
+              // number of visits on your web page. For example: “You
+              // have visited this site N times”.
+
+              var visit = '14 times';
+              alert (visit);
+
+               // TASK NO.3 . Declare a variable called birthYear & assign to it your
+               // birth year. Show the following message in your browser
+
+               var birthYear;
+               birthYear =" I am born in 1995 \n ";
+               var dataType =  "Data type of varaible declare in Number";
+               document.write("<br>"+ "<br>"+  birthYear +"<br>");
+               document.write(dataType );
+
+               // TASK NO.4 A visitor visits an online clothing store
+              //  www.xyzClothing.com . Write a script to store in variables
+             // the following information:
+            // a. Visitor’s name
+           // b. Product title
+          // c. Quantity i.e. how many products a visitor wants to
+         // order
+        // Show the following message in your browser: “John
+       // Doe ordered 5 T-shirt(s) on XYZ Clothing store”
+        var VisitorName;
+        var productsTitles;
+        var quantity;
+        document.write("<br>"+ "<br>"+ "<br>"+ "<b> jhon Doe </b> ordered <b> 5 T-shirt(s) </b> on XYZ Clothing store" );
+               
+     // EXRCISE NO.4 LIGAL AND ILLIGAL VARIABLE
+
+     // TASK NO.1 DECLARE THREE VARIABLE IN ONE STATEMENT.
+      
+     var a = 0, b = 1, c = 2;
+       // TASK NO.2 Declare 5 legal & 5 illegal variable names.
+        // legal varialbs
+        //var $name , name, NAME , myName ,task_one ;
+        // ilegal 
+       // var 1name , 23 , 1NAME , +33name, -4;
+
+        // TASK NO.3 
+        //  Display this in your browser
+        // a) A heading staRting “Rules for naming JS variables”
+        // b) Variable names can only contain ______, ______,
+        // ______ and ______.
+        // For example $my_1stVariable
+        // c) Variables must begin with a ______, ______ or
+        // _____. For example $name, _name or name
+        // d) Variable names are case _________
+        // e) Variable names should not be JS _________
+         
+        var heading = "a)<b> Rules for naming JS variables </b>";
+        var quiz1 =    " b) Variable names can only contain ______ ______  ______ and ______" ;
+        var quiz2 =    "c) Variables must begin with a ______, ______ or _____. For example $name, _name or name";
+        var quiz3 =    " d) Variable names are case _________" ;
+        var quiz4 =    " e)  Variable names should not be JS _________" ;
+        var call_variable = "<br>"+ "<br>"+  "<br>"+  "<br>"+  heading  + "<br>" + "<br>"+ "<br>"+ "<br>"+  quiz1 + "<br>" +   quiz2 +  "<br>" + quiz3 + "<br>" + quiz4 +"<br>";
+        document.write(call_variable);                       
+
+
+                           // EXERCISE NO.5 MATH EXPRESSIONS
+          // TASK NO.1  
+          // Write a program that take two numbers & add them in a
+          // new variable. Show the result in your browser. 
+          var sum = 5 ;
+          var add = 6 ;
+          var total = sum + add ;
+          document.write("<br>" + "Sum of 5 and 6 is" + "&nbsp" + total);
+
+          // TASK NO.2 REPEAT TASK 1 SUBTRACTION MULTIPLICTION DIVISION AND MODULUS.
+
+          var sum = 5 ;
+          var add = 6 ;
+          var total = sum - add ;
+          document.write("<br>" + "Sum of 5 and 6 is" + "&nbsp" + total);
+
+          var sum = 5 ;
+          var add = 6 ;
+          var total = sum * add ;
+          document.write("<br>" + "Subtraction of 5 and 6 is" + "&nbsp" + total);
+
+          var sum = 5 ;
+          var add = 6 ;
+          var total = sum / add ;
+          document.write("<br>" + "division of 5 and 6 is" + "&nbsp" + total);
+
+          var sum = 5 ;
+          var add = 6 ;
+          var total = sum % add ;
+          document.write("<br>" + "modulus of 5 and 6 is" + "&nbsp" + total);
+
+          //TASK NO. 3
+          //DECLARE A VARIABLE 
+          var number;
+          document.write ("<br>" + "<br>" + "Value after variable declar" + "&nbsp" + number);
+          var value = 5 ;
+          document.write ("<br>" + "intial value" + value);
+          var value = 5 ;
+           value++;
+           var someValue = 7 + value;
+           var value2 =  someValue--;
+           var divided = someValue % 3;
+           document.write("<br>" + "value after increment" + "&nbsp" +  value);
+          
+          document.write("<br>" + "value after addition is" + "&nbsp" +value2);
+          document.write("<br>" + "value after decrement is" + "&nbsp" + someValue);
+          document.write("<br>" + "reminder is " + "&nbsp" + divided);
+
+          // TASK NO 4
+          // Cost of one movie ticket is 600 PKR. Write a script to store
+          // ticket price in a variable & calculate the cost of buying 5
+          // tickets to a movie
+
+          var ticket_cost = 600;
+          var buying_ticket = 5;
+          var total_cost = ticket_cost * buying_ticket;
+          document.write("<br>" + "<br>" + "total cost of 5 tickect is" +  "&nbsp" + total_cost +"PKR" );
+          
+           // TASK NO.5 Make a multiplication table
+           
+
+
+
+           
+
+           // TASK NO 6 Temprature convertor
+
+           function cToF(celsius) 
+           {
+             var cTemp = celsius;
+             var cToFahr = cTemp * 9 / 5 + 32;
+             var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+             document.write("<br>" + "<br>" + message);
+           }
+           
+           function fToC(fahrenheit) 
+           {
+             var fTemp = fahrenheit;
+             var fToCel = (fTemp - 32) * 5 / 9;
+             var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+               document.write ("<br>" +  message  +"<br>" );
+           } 
+           cToF(25);
+           fToC(70);
+
+            // TASK NO 7 
+            var price_of_itme_1 = 650;
+            var price_of_itme_2 = 100;
+            var Ordered_quantity_of_item_1 = 3;
+            var Ordered_quantity_of_item_2 = 7;
+            var Shipping_charges = 100;
+            var totalCostOfYourOrder =  price_of_itme_1 * Ordered_quantity_of_item_1 + price_of_itme_2 * Ordered_quantity_of_item_2 + Shipping_charges;
+            document.write ("<br>" + "price_of_itme_1 = 650" + "<br>" +  " quantity_of_item_1 = 3" + "<br>" + "price_of_itme_2"+ "<br>" + "quantity_of_item_2 = 7" + "<br>"+ "ShippingRate = 100"+ "<br>" + "total Cost Of Your Order Is"+ "&nbsp"+ totalCostOfYourOrder +"<br>") ;
+
+           // TASK NO.8 COMPUTE MARKS 
+           document.write("<br>"+ "<b> MARKS SHEET </b>")
+            var total_marks = 980;
+            var marksObtain2Students = 804;
+            var percentage = 100;
+            var obtain_perc = marksObtain2Students *   percentage / total_marks;
+            document.write( "<br>" + "total_marks = 980" + "<br>" + " marksObtain2Students  = 804"+  "<br>" + "percentage =" +"&nbsp" + obtain_perc);
+
+          // TASK NO 9 EXCHANGE CURRENCY
+          var exchangeCurrency = (10 * 104.80) + (25 * 28) ;
+          document.write("<br>"+ "Total currency in PKR" + "&nbsp"+ exchangeCurrency)
+
+         // TASK NO.10 do airthematic
+          var value = (10 + 5 * 10 / 2);
+          document.write ("<br>"+ "airthematic no"+ "&nbsp"+ value);
+
+          //EXERCISE NO 6 TO 9;
+           var a = 10;
+           document.write( "<br>"+" value of is a is "+ a);
+             var b = ++a;
+            document.write("<br>" + "value of ++a is :" + a + "<br>" + "now value of a is"+a);
+            var c = b++;
+            document.write ("<br>"+ b);
+           
+           
+            
+            
+
+
+           
+           
+
+
+
+
+
+          
+          
+
+
+
+
+
+
+
+
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
